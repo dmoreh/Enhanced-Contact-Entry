a3cd1fcdfe980ba3b3e195c8fd2bf9b9cd705a94
Daniel Moreh <dmoreh@gmail.com>
2015-04-03 02:06:03 -0400

    First shot at UI

--- Enhanced Contact Entry/ViewController.swift
+++ Enhanced Contact Entry/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AddressBook
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the default text in notes.
+        let now = NSDate()
+        notesTextView.text = "Met at \(now)" // TODO: Format date appropriately.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func doneButtonPressed(sender: AnyObject) {
+        // Create a new contact. 
+        
+        // Save it. 
+        
+        // Wipe our UI.
+        
+        // Open that new contact's page.
     }
-
-
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // First three fields move to the next one. Last field dismisses keyboard.
+        // TODO: Make social media requests.
+        if textField == firstNameTextField {
+            lastNameTextField.becomeFirstResponder()
+        } else if textField == lastNameTextField {
+            phoneNumberTextField.becomeFirstResponder()
+        } else if textField == phoneNumberTextField {
+            emailAddressTextField.becomeFirstResponder()
+        } else if textField == emailAddressTextField {
+            emailAddressTextField.resignFirstResponder()
+        }
+        
+        return false
+    }
 }
 
